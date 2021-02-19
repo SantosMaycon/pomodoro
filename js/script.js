1e457e7995635a9fdd17be238d5b6432ffb0795e
SantosMaycon <mayconalves.santos14@gmail.com>
2021-02-19 19:53:54 -0300

    add ring animation countdown timer and fix unicode invalid

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ pauseUp.addEventListener("click", () => {
 });
 
 pauseDown.addEventListener("click", () => {
-  if (pauseLabel.innerText > 1)
+  if (pauseLabel.innerText > 3)
     pauseLabel.innerText = Number(pauseLabel.innerText) - 1;
 });
 
@@ -69,10 +69,12 @@ function screenPomodoro() {
     ? ((minute = +workLabel.innerText),
       workStyle(),
       setTimeInTheClock(+workLabel.innerText),
-      addActivetedInLi())
+      addActivetedInLi(),
+      setTimeLimit(+workLabel.innerText, "work"))
     : ((minute = +pauseLabel.innerText),
       pauseStyle(),
-      setTimeInTheClock(+pauseLabel.innerText));
+      setTimeInTheClock(+pauseLabel.innerText),
+      setTimeLimit(+pauseLabel.innerText, "pause"));
   second = 0;
 }
 
@@ -123,6 +125,10 @@ function clock() {
           }
         }
 
+        timePassed = timePassed += 1;
+        timeLeft = TIME_LIMIT - timePassed;
+        setCircleDasharray();
+
         if (minute === 0 && second === 0) {
           icone.className = "icone-triangle";
           clearInterval(interval);
@@ -137,7 +143,7 @@ function clock() {
                 end = false;
               }
             } else screenPomodoro();
-          }, 50);
+          }, 500);
         }
 
         setTimeInTheClock(minute, second);
@@ -225,5 +231,53 @@ function longPauseClock() {
     "actived-long-pause",
   );
   minute = +pauseLabel.innerText * +sessionLabel.innerText;
+  setTimeLimit(minute, "long-pause");
   setTimeInTheClock(minute);
+}
+
+/* Animation */
+const FULL_DASH_ARRAY = 283;
+
+let TIME_LIMIT;
+let timePassed;
+let timeLeft;
+
+function setTimeLimit(time, name) {
+  TIME_LIMIT = time * 60;
+  timePassed = 0;
+  timeLeft = TIME_LIMIT;
+  document.querySelector(".cirlce-svg").innerHTML = `
+  <div class="base-timer">
+    <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <g class="base-timer__circle">
+        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+        <path
+          id="base-timer-path-remaining"
+          stroke-dasharray="283"
+          class="base-timer__path-remaining ${name}"
+          d="
+            M 50, 50
+            m -45, 0
+            a 45,45 0 1,0 90,0
+            a 45,45 0 1,0 -90,0
+          "
+        ></path>
+      </g>
+    </svg>
+  </div>
+  `;
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
 }
